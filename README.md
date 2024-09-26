221dfa5e3c8d64bc3d06d5c0fd7660dd04e2abbe
Ben <bendoepk@gmail.com>
2024-09-26 07:34:57 -0400

    Added README.md

---
+++ README.md
@@ -0,0 +1,28 @@
+Ben's Neovim Config
+===================
+
+Plugins:
+--------
+**Lazy.nvim**
+> Manage plugins
+> Automatically update plugins
+> Lazy load plugins on events
+
+**Mason.nvim**
+> Manage LSP servers
+> Automatically install LSP based on file extension
+> Manage linters and formatters
+
+**LSPZero**
+> Abstraction over Neovim's LSP client to help integrate
+> LSP's with nvim-cmp (autocomplete) and nvim-lspconfig (basic LSP config)
+
+**Harpoon**
+> Easily switch between files while keeping your cursor
+> in the same place that you left it
+
+**Treesitter**
+> Add better syntax highlighting to your code
+>
+>> **Treesitter Playground**
+>> Show a tree structure of your code
--- init.lua
+++ init.lua
@@ -48,14 +48,14 @@ require("lazy").setup({
 			}
 		},
 		{'nvim-treesitter/playground'},
-		{'theprimeagen/harpoon'},
-		{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
-		{'neovim/nvim-lspconfig'},
-		{'hrsh7th/cmp-nvim-lsp'},
-		{'hrsh7th/nvim-cmp'},
-		{'hrsh7th/cmp-buffer'},
-		{'williamboman/mason.nvim'},
-		{'williamboman/mason-lspconfig.nvim'},
+		{'theprimeagen/harpoon'}, --
+		{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'}, --
+		{'neovim/nvim-lspconfig'}, --
+		{'hrsh7th/cmp-nvim-lsp'}, --
+		{'hrsh7th/nvim-cmp'}, --
+		{'hrsh7th/cmp-buffer'}, --
+		{'williamboman/mason.nvim'}, --
+		{'williamboman/mason-lspconfig.nvim'}, --
 		{'L3MON4D3/LuaSnip'},
 		{
 			'windwp/nvim-autopairs',
