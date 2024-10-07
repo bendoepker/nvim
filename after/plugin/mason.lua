b3b6fc35995e8a3f9f5cf37d394adf34e127d4c4
Ben Doepker <bendoepk@gmail.com>
2024-10-07 19:48:49 -0400

    removed language servers

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -2,7 +2,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd', 'kotlin_language_server'},
+  ensure_installed = {'lua_ls', 'rust_analyzer',},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
