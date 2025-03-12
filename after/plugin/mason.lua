caf298f3f8a55e61fa5528252388a4420c05906f
Ben Doepker <bendoepk@gmail.com>
2025-03-12 19:30:10 -0400

    updated meson config and windows incompatability

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -2,11 +2,13 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-<<<<<<< HEAD
-  ensure_installed = {'lua_ls', 'rust_analyzer', 'eslint'},
-=======
-  ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd'},
->>>>>>> origin
+  ensure_installed = {'lua_ls',
+        'clangd', --dependency: unzip
+        'ts_ls',
+        'rust_analyzer',
+        'mesonlsp',
+    },
+
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
