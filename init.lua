9d4586b907401ad87ed06f81ec632be6e6a962ea
Ben Doepker <bendoepk@gmail.com>
2025-04-06 03:43:18 -0400

    added markdown viewer

--- init.lua
+++ init.lua
@@ -66,7 +66,16 @@ require("lazy").setup({
 			-- this is equivalent to setup({}) function
 		},
 		{'mbbill/undotree'},
-		{'tpope/vim-fugitive'}
+		{'tpope/vim-fugitive'},
+        {
+            'MeanderingProgrammer/render-markdown.nvim',
+            dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+            -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+            -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+            ---@module 'render-markdown'
+            ---@type render.md.UserConfig
+            opts = {},
+        }
 	},
   -- Configure any other settings here. See the documentation for more details.
   -- automatically check for plugin updates
