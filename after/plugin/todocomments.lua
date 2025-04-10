d8a89a49c16d4aa555e0f29f069ef22dc25ea4b6
Ben Doepker <bendoepk@gmail.com>
2025-04-10 00:57:34 -0400

    Added more todo-comments

---
+++ after/plugin/todocomments.lua
@@ -0,0 +1,9 @@
+require('todo-comments').setup ({
+    keywords = {
+        SECTION = {
+            icon =  "§ ",
+            color = "#93E9BE",
+            alt = {"SECT", "BREAK"}
+        }
+    }
+})
--- init.lua
+++ init.lua
@@ -69,11 +69,11 @@ require("lazy").setup({
 		{'tpope/vim-fugitive'},
         {
             'MeanderingProgrammer/render-markdown.nvim',
+            event = "BufRead *.md",
             dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
             -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
             -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
             ---@module 'render-markdown'
-            ---@type render.md.UserConfig
             opts = {},
         }
 	},
